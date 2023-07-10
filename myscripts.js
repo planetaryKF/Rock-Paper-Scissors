37566c279ac2f8d627246fb8abba6a56b11ba6af
planetaryKF <110732205+planetaryKF@users.noreply.github.com>
2023-07-09 23:42:09 -0400

    CPU has ability to choose R-P-S

---
+++ myscripts.js
@@ -0,0 +1,24 @@
+
+const options = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+    const computerRandom = options[Math.floor(Math.random() * options.length)]
+    return computerRandom;
+    }
+//allows computer/npc to 'pick' a random choice from options array
+
+
+function playRound(playerSelection, computerSelection) {
+     
+}
+// will be player input vs cpu input
+
+function result(playerSelection, computerSelection) {
+
+}
+//will check the result of playRound and return "you win/lose/tie"
+
+
+
+
+getComputerChoice();
